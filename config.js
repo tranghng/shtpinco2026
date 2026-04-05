db08ce6123046d8f51a8873d6e1659a6c52a3e24
tranghng <tranghoang.snvdn@gmail.com>
2026-04-05 21:19:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   // Dán URL Apps Script sau khi deploy vào đây
   // Ví dụ: 'https://script.google.com/macros/s/AKfycb.../exec'
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbyFaP9Ts0kZPm9_4Wh-pY9dbur2eFxniJNJ8w0rEM1NXQ7Kikbmj4JOIoXTfwON0dSY5A/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyVqNYkd60WEtvYhsB9fB-RcHnaEp6GvzbnGQKoDDQFewdUUZXSrLiofCoYF1XVozUJyg/exec',
 
   EVENT_NAME: 'Hội Nghị Quốc Tế Thường Niên KCNC lần thứ 8',
   EVENT_DATE: '2026-10-15',
